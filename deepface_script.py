0ce266073b23d8c582def562d7cc089774ef843f
Dominik Kurasbediani <dominik_k@cherrypickgames.com>
2022-02-13 14:00:04 +0100

    Update deepface_script.py

--- deepface_script.py
+++ deepface_script.py
@@ -1,8 +1,10 @@
 from deepface import DeepFace
 
-img1_path = "dataset/img1.jpg"
-img2_path = "dataset/img5.jpg"
+# img1_path = "dataset/img1.jpg"
+# img2_path = "dataset/img5.jpg"
 
-result = DeepFace.verify(
-    img1_path=img1_path, img2_path=img2_path, detector_backend="opencv")
-print(result)
+# result = DeepFace.verify(
+#     img1_path=img1_path, img2_path=img2_path, detector_backend="opencv")
+# print(result)
+
+DeepFace.stream("dataset")
