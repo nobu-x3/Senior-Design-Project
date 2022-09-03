ec08c6cac1334404be8b626443ddc36adb0f0734
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2022-09-03 16:15:43 +0200

    Added a save file maker to avoid manually adding 90 students every time

---
+++ Client/ImgCapture/make_save_data.py
@@ -0,0 +1,22 @@
+import json
+student_data = {
+    "ID": 0,
+    "StudentID": "0",
+    "FirstName": "test0",
+    "LastName": "test0",
+    "BaseImage": "",
+    "ImagePath": "",
+    "LastUpdate": "0001-01-01T00:00:00",
+    "Status": False
+  }
+json_array = []
+for i in range(0, 90):
+  temp = student_data.copy()
+  temp["FirstName"]="test"+"{}".format(i)
+  temp["LastName"]="test"+"{}".format(i)
+  temp["StudentID"]="{}".format(i)
+  temp["ID"] = i+1
+  json_array.append(temp)
+
+with open("SaveData", "w") as write:
+  json.dump(json_array, write)
--- Client/ImgCapture/stress_test.py
+++ Client/ImgCapture/stress_test.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 
 procs = []
-for i in range(1):
+for i in range(90):
     proc = subprocess.Popen([sys.executable, 'capture.py', '{}'.format(i)])
     procs.append(proc)
 
